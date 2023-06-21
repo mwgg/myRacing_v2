915ddd62be7e9aca6d14cfccc82e8e70812abc6f
Michael Wolf <m@mw.gg>
2023-06-21 09:11:39 +0300

    Minor navbar styling changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>myRacing - @yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@forevolve/bootstrap-dark@1.1.0/dist/css/bootstrap-dark.min.css" />
-    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css?v=2') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css?v=3') }}" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -24,12 +24,12 @@
     </div>
 </div>
 
-<nav class="shadow-lg sticky navbar navbar-expand-md navbar-dark bg-dark mb-4">
+<div class="shadow-lg sticky navbar navbar-expand-md navbar-dark bg-dark mb-4">
     <div class="container">
         <div class="ml-3 logo">
             <a href="{{ route('dashboard') }}"><img src="/img/logo.png"/></a>
         </div>
-        <div class="navbar">
+        <nav class="navbar">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
                     <a class="nav-link {{ (Route::currentRouteName() == 'dashboard') ? 'active' : '' }}" aria-current="page" href="{{ route('dashboard') }}">Dashboard</a>
@@ -38,9 +38,9 @@
                     <a class="nav-link {{ (Route::currentRouteName() == 'planner') ? 'active' : '' }}" href="{{ route('planner') }}">Planner</a>
                 </li>
             </ul>
-        </div>
+        </nav>
     </div>
-</nav>
+</div>
 
 <main class="container">
     <div class="p-1 rounded">
