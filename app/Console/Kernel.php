53f4e5a8ed0b2f4e15b07a43a074427c56b66059
Michael Wolf <m@mw.gg>
2023-08-11 16:58:22 +0300

    Work queue starting with cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+		$schedule->command('queue:work --stop-when-empty')
+            ->everyMinute()
+            ->withoutOverlapping();
+
         $schedule->command('update:series')
             ->daily();
 
