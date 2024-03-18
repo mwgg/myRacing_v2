5dea6cd8156a1890adc9f63d0ed1f5d4c2bd40de
Michael Wolf <m@mw.gg>
2024-03-18 09:41:32 +0300

    Added track category icon to track/week blocks

--- resources/views/layouts/track.blade.php
+++ resources/views/layouts/track.blade.php
@@ -5,7 +5,16 @@
      data-bs-toggle="tooltip" data-bs-html="true" title="{{ $schedule->tooltipText() }}"
      style="background: linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7) ), url('{{ $schedule->track->image_url }}')">
     <span class="track-name no-select"><b>{{ $schedule->track->name }}</b> {{ $schedule->track->config_name }}</span>
-    <div class="track-week track-ownership no-select {{ (!$schedule->track->free) ? 'track-ownership-red' : 'track-ownership-green' }}" data-track-id="{{ $schedule->track->package_id }}">{{ $schedule->week_number }}</div>
+    <div class="track-week track-ownership no-select {{ (!$schedule->track->free) ? 'track-ownership-red' : 'track-ownership-green' }}" data-track-id="{{ $schedule->track->package_id }}">
+        <div>
+            {{ $schedule->week_number }}
+        </div>
+        <div>
+            <span class="track-category-icon">
+                <x-dynamic-component :component="'license-icons.' . $schedule->track->category_id" />
+            </span>
+        </div>
+    </div>
     <div class="track-logo" style="background: url({{ $schedule->track->logo_url }}) no-repeat;"></div>
     <div class="track-map" style="background: url({{ $schedule->track->map_url }}) no-repeat;"></div>
     @if(!$schedule->track->free)
